6e62d51d7f9ef8beacbf4fba80a92d687077721d
Timoteusliseka <lisekatimoteus24@gmail.com>
2022-04-18 00:16:58 +0700

    Add: Membuat halaman registrasi ref #1

---
+++ resources/views/user/registrasi.blade.php
@@ -0,0 +1,50 @@
+@extends('template.template')
+@section('content')
+<div class="bg3">
+    <div class="card text-center layreg" style="width: 50rem;">
+        <div class="card-body">
+            <h3 class="card-title poppins">Daftar</h3>
+            <form action="/pasien" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="nik" placeholder="NIK" name="nik">
+                    <label for="nik">NIK</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="bpjs" placeholder="No BPJS" name="bpjs">
+                    <label for="bpjs">No BPJS</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="nama" placeholder="Nama Lengkap" name="nama">
+                    <label for="nama">Nama Lengkap</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="jeniskelamin" placeholder="Jenis Kelamin" name="jenis_kelamin">
+                    <label for="jeniskelamin">Jenis Kelamin</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="phone" class="form-control" id="noHP" placeholder="No. HP" name="noHP">
+                    <label for="noHP">No. HP</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="username" placeholder="Username" name="username">
+                    <label for="username">Username</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" id="pass" placeholder="password" name="pass">
+                    <label for="pass">Password</label>
+                </div>
+                <div class="layreg1">
+                    <button type="submit" class="btn btn-2 poppins fw-bold">Daftar</button>
+                </div>
+            </form>
+            <p class="poppins">Website | Copyright Sikesmas 2021</p>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+</div>
+
+@endsection
