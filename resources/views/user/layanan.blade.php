0c53093196d062d120fac4191d17d3e6e5a646e5
abdulazis99 <92225961+abdulazis99@users.noreply.github.com>
2022-05-12 13:29:17 +0700

    add: Membuat halaman pilihan poli (layanan) ref #11

---
+++ resources/views/user/layanan.blade.php
@@ -0,0 +1,171 @@
+@extends('template.template')
+@section('content')
+<section id='layanan' class="vh-80 py-5">
+    <div class="container sm">
+        <h3 class="text-center fw-bold">Unit Layanan Puskesmas</h3>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card" style="max-width: 500px;">
+                    <div class="row g-0">
+                        <div class="col-md-5">
+                            <img src="image/gigi.jpg" class="card-img-top h-100 bw" alt="...">
+                        </div>
+                        <div class="col-md-7">
+                            <div class="card-body" style="background: #DA2871;">
+                                <h5 class="card-title poppins fw-bold text-light">Poli Gigi</h5>
+                                <p class="card-text poppins text-light">Ini adalah contoh laman. Ini berbeda dengan
+                                    posting
+                                    blog karena akan
+                                    tetap berada di satu tempat dan akan muncul di website.</p>
+                                <a href="/keluhan" class="btn btn-outline-light">Booking Antrian</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="max-width: 500px;">
+                    <div class="row g-0">
+                        <div class="col-md-5">
+                            <img src="image/anak.jpg" class="card-img-top h-100 bw" alt="...">
+                        </div>
+                        <div class="col-md-7">
+                            <div class="card-body" style="background: #DA2871;">
+                                <h5 class="card-title poppins fw-bold text-light">Poli Anak</h5>
+                                <p class="card-text poppins text-light">Ini adalah contoh laman. Ini berbeda dengan
+                                    posting
+                                    blog karena akan
+                                    tetap berada di satu tempat dan akan muncul di website.</p>
+                                <a href="/keluhan" class="btn btn-outline-light">Booking Antrian</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="max-width: 500px;">
+                    <div class="row g-0">
+                        <div class="col-md-5">
+                            <img src="image/mata.jpg" class="card-img-top h-100 bw" alt="...">
+                        </div>
+                        <div class="col-md-7">
+                            <div class="card-body" style="background: #DA2871;">
+                                <h5 class="card-title poppins fw-bold text-light">Poli Mata</h5>
+                                <p class="card-text poppins text-light">Ini adalah contoh laman. Ini berbeda dengan
+                                    posting
+                                    blog karena akan
+                                    tetap berada di satu tempat dan akan muncul di website.</p>
+                                <a href="/keluhan" class="btn btn-outline-light">Booking Antrian</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="max-width: 500px;">
+                    <div class="row g-0">
+                        <div class="col-md-5">
+                            <img src="image/umum.jpg" class="card-img-top h-100 bw" alt="...">
+                        </div>
+                        <div class="col-md-7">
+                            <div class="card-body" style="background: #DA2871;">
+                                <h5 class="card-title poppins fw-bold text-light">Poli Umum</h5>
+                                <p class="card-text poppins text-light">Ini adalah contoh laman. Ini berbeda dengan
+                                    posting
+                                    blog karena akan
+                                    tetap berada di satu tempat dan akan muncul di website.</p>
+                                <a href="/keluhan" class="btn btn-outline-light">Booking Antrian</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="max-width: 500px;">
+                    <div class="row g-0">
+                        <div class="col-md-5">
+                            <img src="image/kandungan.jpg" class="card-img-top h-100 bw" alt="...">
+                        </div>
+                        <div class="col-md-7">
+                            <div class="card-body" style="background: #DA2871;">
+                                <h5 class="card-title poppins fw-bold text-light">Poli Kandungan</h5>
+                                <p class="card-text poppins text-light">Ini adalah contoh laman. Ini berbeda dengan
+                                    posting
+                                    blog karena akan
+                                    tetap berada di satu tempat dan akan muncul di website.</p>
+                                <a href="/keluhan" class="btn btn-outline-light">Booking Antrian</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="max-width: 500px;">
+                    <div class="row g-0">
+                        <div class="col-md-5">
+                            <img src="image/tht.jpg" class="card-img-top h-100 bw" alt="...">
+                        </div>
+                        <div class="col-md-7">
+                            <div class="card-body" style="background: #DA2871;">
+                                <h5 class="card-title poppins fw-bold text-light">Poli THT</h5>
+                                <p class="card-text poppins text-light">Ini adalah contoh laman. Ini berbeda dengan
+                                    posting
+                                    blog karena akan
+                                    tetap berada di satu tempat dan akan muncul di website.</p>
+                                <a href="/keluhan" class="btn btn-outline-light">Booking Antrian</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container sm margin" style="background: #333333;">
+        <h3 class="poppins fw-bold text-light text-center">Antrian Poli</h3>
+        <div class="row" style="padding-left:1%;">
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:60px">15</h1>
+            </div>
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:60px">15</h1>
+            </div>
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:60px">15</h1>
+            </div>
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:60px">15</h1>
+            </div>
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:60px">15</h1>
+            </div>
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:60px">15</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container sm layout3" style="background:#DA2871;">
+        <div class="row" style="padding-left:1%;">
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:18px">Poli Gigi</h1>
+            </div>
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:18px">Poli Anak</h1>
+            </div>
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:18px">Poli Mata</h1>
+            </div>
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:18px">Poli Umum</h1>
+            </div>
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:18px">Poli Kandungan</h1>
+            </div>
+            <div class="col-2">
+                <h1 class="poppins text-light fw-bold text-center" style="font-size:18px">Medical Check-up</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
